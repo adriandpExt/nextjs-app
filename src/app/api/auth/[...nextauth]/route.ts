ec2fdff5728fd846ba55ac38a3242189c155f417
adriandpExt <adriandp.ext@gmail.com>
2024-08-04 12:56:08 +0800

    split

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,6 @@
 import NextAuth from "next-auth";
 
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.AUTH_GITHUB_ID ?? "",
-      clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-  ],
-};
+import { authOptions } from "@/app/auth";
 
 export const handlers = NextAuth(authOptions);
 
